70e71e919608e1f33a77adfafe638a9a2aeddcbc
kiril <kirill1601krk@gmail.com>
2024-03-30 14:00:25 +0500

    add parsers

--- cmd/afisha_parser/app.py
+++ cmd/afisha_parser/app.py
@@ -4,24 +4,34 @@ import time
 from kafka import KafkaProducer
 import os
 from dotenv import load_dotenv
+import json
+import asyncio
 
-def main():
+async def main():
     load_dotenv()
+    u = os.getenv('URLS').split(';')
     kfk = os.getenv('KAFKA_URL')
-    producer = KafkaProducer(bootstrap_servers=[kfk],
-                             value_serializer=lambda x:
-                             dumps(x).encode('utf-8'))
+    t = int(os.getenv('TIMEOUT'))
+    pagesCount = int(os.getenv('PAGES'))
+    topic = os.getenv('KAFKA_TOPIC')
+    if t is None:
+        t = 60*60*1
+    else:
+        t = int(t)
+    producer = KafkaProducer(bootstrap_servers=[kfk], value_serializer=lambda m: json.dumps(m).encode('ascii'))
     print('start')
-    url = os.getenv('URL')
     while True:
-        print('process')
-        process(url)
-        time.sleep(60 * 60 * 8)
+        await asyncio.gather(
+            *map(lambda x: process_afisha(x, pagesCount, producer, topic), u)
+        )
+        print('fall asleep for {} seconds'.format(t))
+        time.sleep(t)
 
 
-def process(url):
-    for i in range(100):
-        url = 'https://www.afisha.ru/prm/{}/page{}'.format(url, i + 1)
+async def process_afisha(u, pagesCount, producer, topic):
+    print('processing https://www.afisha.ru/prm/{}'.format(u))
+    for i in range(pagesCount):
+        url = 'https://www.afisha.ru/prm/{}/page{}'.format(u, i + 1)
         res = requests.get(url,
                            headers={
                                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
@@ -39,20 +49,31 @@ def process(url):
                            }
                            )
         if not res.ok:
+            print(res.status_code, url)
             break
-        soup = BeautifulSoup(res.text)
+        soup = BeautifulSoup(res.text, features="html.parser")
         list = soup.find_all("div", {'aria-label': 'Список'})[0]
         items = list.find_all("div", {'role': 'listitem', 'data-test': 'ITEM'})
-        last_len = len(items)
         data = []
         for i in items:
             title = i.get('title')
             notice = i.find_all("div", {'data-test': 'ITEM-META ITEM-NOTICE'})[0].text
             link = "https://www.afisha.ru" + i.find_all('a', {'data-test': 'LINK ITEM-NAME ITEM-URL'})[0].get('href')
-            data.append({"title": title, "notice": notice, "link": link})
+            source = "https://www.afisha.ru"
+            image = i.find_all("img", { 'data-test': "IMAGE ITEM-IMAGE" })[0].get('src')
+            data.append({"title": title, "notice": notice, "link": link, "source": source, "image": image})
 
-        producer.send('numtest', value=data)
-        time.sleep(20)
+        producer.send(topic, data).add_callback(on_send_success).add_errback(on_send_error)
+        print("sent {} items in data".format(len(data)))
+        await asyncio.sleep(20)
 
+def on_send_success(record_metadata):
+    print(record_metadata.topic)
+    print(record_metadata.partition)
+    print(record_metadata.offset)
 
-main()
+def on_send_error(excp):
+    print('error: {}'.format(excp))
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ cmd/ya_afisha_parser/app.py
@@ -0,0 +1,83 @@
+import requests
+import time
+from kafka import KafkaProducer
+import os
+from dotenv import load_dotenv
+import json
+import asyncio
+
+async def main():
+    load_dotenv()
+    types = os.getenv('EVENT_TYPES').split(';') # spectacle;all-events-cinema
+    kfk = os.getenv('KAFKA_URL')
+    t = int(os.getenv('TIMEOUT'))
+    topic = os.getenv('KAFKA_TOPIC')
+    pagesCount = int(os.getenv('PAGES'))
+    if t is None:
+        t = 60*60*1
+    else:
+        t = int(t)
+    producer = KafkaProducer(bootstrap_servers=[kfk], value_serializer=lambda m: json.dumps(m).encode('ascii'))
+    print('start')
+    while True:
+        await asyncio.gather(
+            *map(lambda x: process_afisha(x, pagesCount, producer, topic), types)
+        )
+        print('fall asleep for {} seconds'.format(t))
+        time.sleep(t)
+
+
+async def process_afisha(type, pagesCount, producer, topic):
+    print('processing https://afisha.yandex.ru/api/events/selection/{}'.format(type))
+    for i in range(1, pagesCount):
+        offset = 12 * (i - 1)
+        url = 'https://afisha.yandex.ru/api/events/selection/{}?limit=12&offset={}&city=perm'.format(type, offset)
+        res = requests.get(url, headers={
+            'Accept':	'application/json, text/javascript, */*; q=0.01',
+            'Accept-Encoding':	'gzip, deflate, br',
+            'Accept-Language':	'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+            'Cache-Control':	'no-cache',
+            'Connection':	'keep-alive',
+            'Host':	'afisha.yandex.ru',
+            'Pragma':	'no-cache',
+            'Referer':	'https://afisha.yandex.ru/perm/selections/{}?page={}'.format(type, i),
+            'Sec-Fetch-Dest':	'empty',
+            'Sec-Fetch-Mode':	'no-cors',
+            'Sec-Fetch-Site':	'same-origin',
+            'User-Agent':	'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0',
+            'x-force-cors-preflight':	'1',
+            'X-Parent-Request-Id':	'1711781556060751-13537792777347991917',
+            'X-Requested-With':	'XMLHttpRequest',
+            'X-Retpath-Y':	'https://afisha.yandex.ru/perm/selections/{}?page={}'.format(type, i),
+        })
+        if not res.ok:
+            print(url)
+            break
+        data = res.json()
+        events = []
+        for i in data['data']:
+            link = "afisha.yandex.ru{}".format(i['event']['url'])
+            notice = "с {} по {}, {}".format(i['scheduleInfo']['dateStarted'], i['scheduleInfo']['dateEnd'], i['scheduleInfo']['placePreview'])
+            image = None
+            try:
+                image = i['event']['image']['sizes']['eventCover']['url']
+            except TypeError:
+                print(i['event'])
+            title = i['event']['title']
+            events.append({"title": title, "notice": notice, "link": link, "image": image, "source": "afisha.yandex.ru"})
+        
+        producer.send(topic, data).add_callback(on_send_success).add_errback(on_send_error)
+        await asyncio.sleep(5)
+
+
+def on_send_success(record_metadata):
+    print(record_metadata.topic)
+    print(record_metadata.partition)
+    print(record_metadata.offset)
+
+def on_send_error(excp):
+    print('error: {}'.format(excp))
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
